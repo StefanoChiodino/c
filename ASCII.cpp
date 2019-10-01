aa9c8ceddc708807ddcaeff57f892b442f82fb26
Sonali1772 <56030380+Sonali1772@users.noreply.github.com>
2019-10-01 19:14:57 +0530

    ASCII.cpp

--- ASCII.cpp
+++ ASCII.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    unsigned int i;
-    for(i=0;1<127;i++){
-                         printf("%d: %c\n",i,i);
+    unsigned int j;
+    for(j=0;1<127;j++){
+                         printf("%d: %c\n",j,j);
     fflush(stdin);getchar();}
 }
